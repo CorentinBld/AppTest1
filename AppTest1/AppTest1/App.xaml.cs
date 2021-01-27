fa47ba6fadd5e0f244677cbca6c12e454d3813a9
Corentin Boulard <corentin.boulard@gmail.com>
2021-01-27 19:06:23 +0100

    init

---
+++ AppTest1/AppTest1/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Diagnostics;
+using Xamarin.Forms.Xaml;
+
+namespace AppTest1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage =new NavigationPage (new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            Debug.WriteLine("OnStart");
+            if (Current.Properties.ContainsKey("MainPageID"))
+            {
+                var id = Current.Properties["MainPageID"];
+                Debug.WriteLine("OnStart-" + id);
+            }
+        }
+
+        protected override void OnSleep()
+        {
+            Debug.WriteLine("OnSleep");
+        }
+
+        protected override void OnResume()
+        {
+            Debug.WriteLine("OnResume");
+            if (Current.Properties.ContainsKey("MainPageID"))
+            {
+                var id = Current.Properties["MainPageID"];
+                Debug.WriteLine("OnResume-" + id);
+            }
+        }
+    }
+}
