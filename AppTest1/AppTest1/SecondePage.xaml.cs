1bdb887db116b503e7c19108b930da8507f3f4d6
Corentin Boulard <corentin.boulard@gmail.com>
2021-01-28 16:45:37 +0100

    init

--- AppTest1/AppTest1/SecondePage.xaml.cs
+++ AppTest1/AppTest1/SecondePage.xaml.cs
@@ -16,5 +16,54 @@ namespace AppTest1
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked_Select_RED_Color(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GreenColorSelectButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BlueColorSelectButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void CyanColorSelectButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void YellowColorSelectButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void PurpleColorSelectButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void WhiteColorSelectButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void BlinkColorSelectButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void MarcheSelectButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void ArretSelectButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MoteurSelectButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
     }
 }
+
